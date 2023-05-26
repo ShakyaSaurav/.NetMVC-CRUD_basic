210beb9d04095486ba95e5d85b57082cc5de2c8b
ShakyaSaurav <sauravshakya2015@gmail.com>
2023-05-26 20:26:47 +0545

    complete

---
+++ ASPNETMVCCRUD/Migrations/20230526051415_corrected-migration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPNETMVCCRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class correctedmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Emmployees",
+                table: "Emmployees");
+
+            migrationBuilder.RenameTable(
+                name: "Emmployees",
+                newName: "Employees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees");
+
+            migrationBuilder.RenameTable(
+                name: "Employees",
+                newName: "Emmployees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Emmployees",
+                table: "Emmployees",
+                column: "Id");
+        }
+    }
+}
